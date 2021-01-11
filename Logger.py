8ff3c9e863d6066d7429016d05e1403843cdde5b
MaxsCorrea <correamaximiliano10@gmail.com>
2021-01-11 13:10:30 -0300

    Updating Data
    Updating data on Monitor.py and rename Dashboard to services.json

---
+++ Logger.py
@@ -0,0 +1,17 @@
+import logging
+
+# Logger configuration
+LOG_FORMAT = "%(levelname)s %(asctime)s - %(message)s"
+logging.basicConfig(filename= r"C:\Users\Maximiliano\Documents\TrainingProject-main\TrainingProject-main\Monitoring.log",
+                    level= logging.DEBUG, 
+                    format=LOG_FORMAT, 
+                    filemode='w')
+logger = logging.getLogger()
+
+#Messages
+logger.debug("Debuggin services...")
+logger.info("The service is health")
+logger.warning("A service reboot will be good")
+logger.error("Some Services are not working correctly")
+logger.critical("service DOWN!!!")
+
--- Monitor.py
+++ Monitor.py
@@ -1,1 +1,21 @@
+import json
+import logging
 
+
+
+
+#open json data
+with open('services.json') as f:
+    data = json.load(f)
+
+#reading json data
+for service in data["services"]:
+    print(service['name'], 
+    service['service'],
+    service['disk usage']) 
+
+    if service ['disk usage'] < "50%":
+        print("disk is working well")
+
+    else:
+        print("overheated disk")   
