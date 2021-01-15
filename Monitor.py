48910e442f0eb4e8d0c340c734eacd7e9c8f250c
Maximiliano Correa <correamaximiliano10@gmail.com>
2021-01-15 11:13:52 -0300

    adding argparse parameters

--- Monitor.py
+++ Monitor.py
@@ -1,5 +1,13 @@
 import json
+import argparse
 import logging
+
+
+#argparse config
+parser = argparse.ArgumentParser()
+
+
+
 
 
 #open json data
@@ -8,9 +16,11 @@ with open('services.json') as f:
 
 #reading json data
 for service in data["services"]:
-    print(service['name'], 
-    service['service'],
-    service['disk usage']) 
+    print(service['name'])
+    print(service['service'])
+    print(service['url'])
+    print(service['disk usage'])
+ 
 
     if service ['disk usage'] < "50%":
         print("disk is working well")
