ad72780a15f71c0df4bc6d6b8614fd4695974c3a
Maximiliano Correa <correamaximiliano10@gmail.com>
2021-01-21 11:09:37 -0300

    adding code for the logger

--- Logger.py
+++ Logger.py
@@ -1,4 +1,13 @@
 import logging
+import json
+
+
+#set up json file
+def get_data():
+    with open('services.json') as f:
+        data = json.load(f)
+        return data
+data = get_data()
 
 # Logger configuration
 LOG_FORMAT = "%(levelname)s %(asctime)s - %(message)s"
@@ -10,8 +19,24 @@ logger = logging.getLogger()
 
 #Messages
 logger.debug("Debuggin services...")
-logger.info("The service is health")
-logger.warning("A service reboot will be good")
-logger.error("Some Services are not working correctly")
-logger.critical("service DOWN!!!")
+
+
+for s in data["services"]:
+    sr = (s["service"])
+    if "stopped" == sr:
+        logger.warning(f'Service {s["name"]} service stopped')
+    elif "error" == sr:
+        logger.critical(f'Service {s["name"]} service error. Review ASAP')
+    elif "running" == sr:
+        logger.info(f'Service {s["name"]} service running')    
+
+for s in data["services"]:
+    du = float(s["disk usage"][:-1])
+    if 50 < du:
+     logger.error(f'Service {s["name"]} disk usage {du}% overheated disk')
+    elif 40 < du < 50:
+     logger.warning(f'Service {s["name"]} disk usage {du}% please check the disk')
+    elif 40:
+     logger.info(f'Service {s["name"]} disk usage {du}%')
+
 
